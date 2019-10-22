8922c8a694df764eb3082d1af2fd827c035ecf05
AlenaLoik <CrazyBeeLviv@gmail.com>
2019-10-22 14:57:26 +0300

    fixed img and header

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("myHeader").style.height = "215px";
+    document.getElementById("myHeader").style.height = "150px";
   } else {
-    document.getElementById("myHeader").style.height = "265px";
+    document.getElementById("myHeader").style.height = "200px";
   }
 }
 
